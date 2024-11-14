bc95cd9a27d7727effa279ce8d6c75df34d22186
Abulaf1a <peterrazzell688@gmail.com>
2024-11-14 17:57:03 +0000

    feat: implement simple main method to demonstrate rover movement alongside tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,42 @@
+import data.Controller;
 import data.Direction;
+import data.Instruction;
+import data.Position;
 import input.InputParser;
 
 public class Main {
 
-    InputParser inputParser = new InputParser();
+    public static void main(String[] args) {
+        InputParser inputParser = new InputParser();
+
+        Controller controller = new Controller(new int[]{5,7}, new Position(1, 2, Direction.N));
+
+        System.out.println(controller.renderScene());
+        System.out.println("rover is at position: " + controller.getRoverPosition());
+        System.out.println();
+
+        controller.moveRover(Instruction.M);
+        System.out.println(controller.renderScene());
+        System.out.println("rover is at position: " + controller.getRoverPosition());
+        System.out.println();
+
+        controller.moveRover(Instruction.M);
+        System.out.println(controller.renderScene());
+        System.out.println("rover is at position: " + controller.getRoverPosition());
+        System.out.println();
+
+        controller.moveRover(Instruction.L);
+        System.out.println(controller.renderScene());
+        System.out.println("rover is at position: " + controller.getRoverPosition());
+        System.out.println();
+
+        controller.moveRover(Instruction.M);
+        System.out.println(controller.renderScene());
+        System.out.println("rover is at position: " + controller.getRoverPosition());
+        System.out.println();
+
+
+    }
 
 
 
