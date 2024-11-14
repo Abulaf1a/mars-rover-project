ca5da530f6284a2ef8b1001c708308455da5d456
Abulaf1a <peterrazzell688@gmail.com>
2024-11-14 16:46:25 +0000

    test: implement basic test functionality for Plateau class

--- src/test/java/data/PlateauTest.java
+++ src/test/java/data/PlateauTest.java
@@ -1,11 +1,37 @@
-import data.Plateau;
+package data;
+
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlateauTest {
 
     @org.junit.jupiter.api.Test
-    void getPlateau_() {
+    void getPlateau_validInput() {
+        //Arrange
+        char[][] expected = new char[3][3];
+        Plateau plateau = new Plateau(3,3);
+
+        //Act
+        char[][] actual = plateau.getPlateau();
+
+        //Assert
+        assertArrayEquals(expected,actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPlateau_emptyPlateau() {
+        //Arrange
+        char[][] expected = new char[0][0];
+        Plateau plateau = new Plateau(0,0);
+
+        //Act
+        char[][] actual = plateau.getPlateau();
+
+        //Assert
+        assertArrayEquals(expected,actual);
+
     }
 
     @org.junit.jupiter.api.Test
@@ -18,8 +44,38 @@ class PlateauTest {
         System.out.println(plateauDisplay);
 
         //Assert
-        assertEquals("00000\n00000\n00000\n00000\n00000\n", plateauDisplay);
+        assertEquals("\0\0\0\0\0\n\0\0\0\0\0\n\0\0\0\0\0\n\0\0\0\0\0\n\0\0\0\0\0\n", plateauDisplay);
 
+
+    }
+
+    @Test
+    void isValidPosition_invalidPosition() {
+        //Arrange
+        Position position = new Position(100,100,Direction.N);
+
+        Plateau plateau = new Plateau(5,5);
+
+        //Act
+        boolean actual = plateau.isValidPosition(position);
+
+        //Assert
+        assertFalse(actual);
+
+    }
+
+    @Test
+    void isValidPosition_validPosition() {
+        //Arrange
+        Position position = new Position(3,2,Direction.N);
+
+        Plateau plateau = new Plateau(5,5);
+
+        //Act
+        boolean actual = plateau.isValidPosition(position);
+
+        //Assert
+        assertTrue(actual);
 
     }
 }
