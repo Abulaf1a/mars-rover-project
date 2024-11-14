839b876de956e3bd752f9806b26311daf8023d03
Abulaf1a <peterrazzell688@gmail.com>
2024-11-14 10:27:04 +0000

    test: implement test for Plateau class printPlateu_testExpectedInput method

---
+++ src/test/java/PlateauTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlateauTest {
+
+    @org.junit.jupiter.api.Test
+    void getPlateau_() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printPlateau_testExpectedInput() {
+        //Arrange
+        Plateau plateau = new Plateau(5,5);
+
+        //Act
+        String plateauDisplay = plateau.toString();
+        System.out.println(plateauDisplay);
+
+        //Assert
+        assertEquals("00000\n00000\n00000\n00000\n00000\n", plateauDisplay);
+
+
+    }
+}
