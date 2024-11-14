0bb31aa09dfae5cb53a0146650be17fad57d92d6
Abulaf1a <peterrazzell688@gmail.com>
2024-11-14 16:29:35 +0000

    feat: implement rotate methods for Direction enum

--- src/main/java/data/Direction.java
+++ src/main/java/data/Direction.java
@@ -2,8 +2,43 @@ package data;
 
 public enum Direction {
 
-    N,
-    S,
-    E,
-    W
+    N {
+        @Override
+        public Direction rotate(Instruction instruction) {
+            if(instruction == Instruction.R){
+                return E;
+            }
+            return W;
+        }
+    },
+    S {
+        @Override
+        public Direction rotate(Instruction instruction) {
+            if(instruction == Instruction.R){
+                return W;
+            }
+            return E;
+        }
+    },
+    E {
+        @Override
+        public Direction rotate(Instruction instruction) {
+            if(instruction == Instruction.R){
+                return S;
+            }
+            return N;
+        }
+    },
+    W {
+        @Override
+        public Direction rotate(Instruction instruction) {
+            if(instruction == Instruction.R){
+                return N;
+            }
+            return S;
+        }
+    };
+
+
+    public abstract Direction rotate(Instruction instruction);
 }
