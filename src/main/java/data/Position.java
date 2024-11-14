4636ec1100f90ea34cdd7cd55b8583855e4cf11e
Abulaf1a <peterrazzell688@gmail.com>
2024-11-14 17:12:42 +0000

    feat: implement methods in Position class to calculate a new position given an instruction methods are: calculateUpdatedPosition, calculateMovePosition, calculateRotatePosition

--- src/main/java/data/Position.java
+++ src/main/java/data/Position.java
@@ -12,6 +12,41 @@ public class Position {
         this.facing = facing;
     }
 
+    public Position calculateUpdatedPosition(Instruction instruction){
+        /*this position has not yet been checked by the plateau class for obstacles or falling off the edge of the map
+        this logic calculates the new position, the controller then passes that position to the plateau,
+        which returns whether the new position is valid, only then is this position updated with the new position calculated here.*/
+        Position uncheckedUpdatePosition = new Position(0,0,Direction.N);
+        switch (instruction){
+            case M -> uncheckedUpdatePosition = calculateMovePosition();
+            case L -> uncheckedUpdatePosition = calculateRotatePosition(Instruction.L);
+            case R -> uncheckedUpdatePosition = calculateRotatePosition(Instruction.R);
+        }
+
+        return uncheckedUpdatePosition;
+    }
+
+    private Position calculateMovePosition(){
+        Position tempPosition = new Position(x, y, facing);
+
+        switch(facing){
+            case N -> tempPosition.x--;
+            case E -> tempPosition.y++;
+            case S -> tempPosition.x++;
+            case W -> tempPosition.y--;
+        }
+        return tempPosition;
+
+    }
+
+    private Position calculateRotatePosition(Instruction instruction){
+
+        Position tempPosition = new Position(x, y, facing);
+        tempPosition.facing = facing.rotate(instruction);
+        return tempPosition;
+
+    }
+
     @Override
     public String toString() {
         return "Position{" +
@@ -32,4 +67,7 @@ public class Position {
     public Direction getFacing() {
         return facing;
     }
+
+
+
 }
