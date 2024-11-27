3f6b8ac13407d1e2277799bab3c33458d8e64acc
Abulaf1a <peterrazzell688@gmail.com>
2024-11-27 18:15:58 +0000

    refactor: change UI class to reflect new Rover class
    separation of Position data and logic into Rover class, UI class altered to take different inputs

--- src/main/java/userinterface/UI.java
+++ src/main/java/userinterface/UI.java
@@ -1,45 +1,143 @@
 package userinterface;
 
-import data.Plateau;
-import data.Position;
-import data.Returnable;
+import data.*;
+import input.InputParser;
+import logic.Rover;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UI {
 
+    private static InputParser inputParser = new InputParser();
+
     //Takes the rendered scene from the
+    public int[] getPlateauSizeInput(){
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            try{
+
+                System.out.println("Please enter plateau size ");
+
+                int[] size = inputParser.getPlateauSize(scanner.nextLine());
+                return size;
+            } catch (Exception e) {
+                System.out.println();
+            }
+        }
+    }
+
+
+    public Position getStartPosition(){
+        while(true){
+            try{
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Please enter rover start position");
+
+                Position position = inputParser.getRoverStartPosition(scanner.nextLine());
+
+                return position;
+            }
+            catch (Exception e){
+                System.out.println();
+            }
+        }
+    }
+
+    public Instruction[] getInstructions(){
+
+        while(true){
+            try{
+                System.out.println("Please enter instructions:");
+
+                Scanner scanner = new Scanner(System.in);
+
+                return inputParser.getInstructions(scanner.nextLine());
+            } catch (Exception e) {
+                System.out.println();
+            }
+        }
+    }
+
+
+    //TODO use methods to update positions of rover according to instructions
     public void renderScene(ArrayList<Returnable> arr){
 
         try{
-            Plateau plateau = validatePlateau(arr.get(0));
-            Position roverPosition = validatePosition(arr.get(1));
+            Plateau plateau = validatePlateau(arr.get(0)); //instanceof
+            Rover rover = validateRover(arr.get(1));
 
             StringBuilder sceneBuilder = new StringBuilder();
 
+            String black = "\u001B[30m";
+            String yellow = "\u001B[33m";
             String red = "\u001B[31m";
             String background = " \u001B[43m";
             String reset = "\u001B[0m";
 
             for (int i = plateau.getY() -1; i > -1; i--) {
-                sceneBuilder.append(red + background);
+                sceneBuilder.append( background);
 
                 for (int j = 0; j < plateau.getX(); j++) {
-                    if(j == roverPosition.getX() && i == roverPosition.getY()){
-                        sceneBuilder.append("\u001B[30m");
 
-                        sceneBuilder.append('#');
-                        sceneBuilder.append(red);
+                    if(rover.getPosition().getFacing() == Direction.E || rover.getPosition().getFacing() == Direction.W){
+                        if(j == rover.getPosition().getX() && i == rover.getPosition().getY() - (rover.getBody().length/2)){
+                            sceneBuilder.append(black);
+                            sceneBuilder.append(rover.getBody()[0]);
+                            sceneBuilder.append(yellow);
+                            sceneBuilder.append(background);
 
+                            j++;
+                        }
+                        else if(j == rover.getPosition().getX() && i == rover.getPosition().getY()){
+                            sceneBuilder.append(black);
+                            sceneBuilder.append(rover.getBody()[1]);
+                            sceneBuilder.append(yellow);
+                            sceneBuilder.append(background);
+
+                            j++;
+                        }
+                        else if(j == rover.getPosition().getX() && i == rover.getPosition().getY() + (rover.getBody().length/2)){
+                            sceneBuilder.append(black);
+                            sceneBuilder.append(rover.getBody()[2]);
+                            sceneBuilder.append(yellow);
+                            sceneBuilder.append(background);
+
+                            j++;
+                        }
+                        else{
+                            if(plateau.getPlateau()[j][i] == '@'){
+                                sceneBuilder.append(red);
+                            }
+                            sceneBuilder.append(plateau.getPlateau()[j][i]);
+                            sceneBuilder.append(yellow);
+                        }
                     }
-                    else{
-                        sceneBuilder.append(plateau.getPlateau()[j][i]);
+                    else if(rover.getPosition().getFacing() == Direction.N || rover.getPosition().getFacing() == Direction.S){
+                        if(j == rover.getPosition().getX()-(rover.getBody().length/2) && i == rover.getPosition().getY()){
+                            sceneBuilder.append(black);
+                            for(char c: rover.getBody()){
+                                sceneBuilder.append(c);
+                            }
+                            j+=rover.getBody().length;
+                            sceneBuilder.append(yellow);
+                            sceneBuilder.append(background);
+
+                        }
+
+                        else{
+                            if(plateau.getPlateau()[j][i] == '@'){
+                                sceneBuilder.append(red);
+                            }
+                            sceneBuilder.append(plateau.getPlateau()[j][i]);
+                            sceneBuilder.append(yellow);
+                        }
                     }
                 }
                 sceneBuilder.append(reset);
                 sceneBuilder.append("\n");
-
-
             }
             System.out.println(sceneBuilder.toString());
         }
@@ -49,10 +147,10 @@ public class UI {
 
     }
 
-    private Position validatePosition(Returnable position) throws IllegalArgumentException{
+    private Rover validateRover(Returnable rover) throws IllegalArgumentException{
 
-        if (position instanceof  Position){
-            return (Position) position;
+        if (rover instanceof Rover){
+            return (Rover) rover;
         }
         else{
             throw new IllegalArgumentException("Unable to convert returnable back to position");
