5d73894614911699c77aedd6fd103a7b450a2c37
Abulaf1a <peterrazzell688@gmail.com>
2024-11-27 17:16:17 +0000

    feat: implement obstacles

--- src/main/java/data/Plateau.java
+++ src/main/java/data/Plateau.java
@@ -1,15 +1,61 @@
 package data;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+
+
 public class Plateau implements Returnable {
 
+    //TODO x and y values can become PlateauSize
     private int x;
     private int y;
     private char[][] plateau;
+    private ArrayList<int[]> obstacles;
 
+    //TODO PlateauSize could be the constructor and getters for coordinates
+
+    //TODO -- separate out logic and data layers, eg. data layer for plateau size
+    public Plateau(int x, int y, Position roverStartPosition) {
+        this.x = x;
+        this.y = y;
+        plateau = new char[x][y];
+
+        generateObstacles(roverStartPosition);
+
+    }
+
+    //Override constructor for previous test compatability
     public Plateau(int x, int y) {
         this.x = x;
         this.y = y;
         plateau = new char[x][y];
+
+        generateObstacles(new Position(0,0,Direction.N));
+    }
+
+    private void generateObstacles(Position roverStartPosition){ //rover position NOT currently used!!
+
+        Random rand = new Random();
+
+        for (int i = 0; i < x/2; i++) { //possible error if the plateau is too small?
+            int[] temp = new int[]{rand.nextInt(x), rand.nextInt(y)};
+
+            plateau[temp[0]][temp[1]] = '@';
+        }
+    }
+
+    public boolean isValidPosition(Position position){
+
+        int checkX = position.getX();
+        int checkY = position.getY();
+
+        if(0 < checkY && checkY < y && 0 < checkX && checkX < x){
+            System.out.println("location contains rock!");
+            return plateau[checkX][checkY] != '@';
+        }
+        return false;
+
     }
 
     public int getX() {
@@ -22,25 +68,5 @@ public class Plateau implements Returnable {
 
     public char[][] getPlateau(){
         return plateau;
-    }
-
-    //Unused except in test functionality has been moved to controller class.
-    @Override
-    public String toString() {
-
-        StringBuilder plateauDisplay = new StringBuilder();
-        for (int i = y; i > 0; i--) { // y axis
-            for (int j = 0; j < x; j++) {
-
-                plateauDisplay.append(plateau[j][i]); //i = y, j = x
-            }
-            plateauDisplay.append("\n");
-        }
-        return plateauDisplay.toString();
-
-    }
-
-    public boolean isValidPosition(Position position){
-        return(position.getY() <= y && position.getX() <= x);
     }
 }
